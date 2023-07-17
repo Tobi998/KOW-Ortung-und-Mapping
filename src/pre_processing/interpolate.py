310dfea8e3d79e547eedc37cb745fc4af1a97e60
Tobi998 <47536855+Tobi998@users.noreply.github.com>
2023-07-17 17:47:16 +0200

    Interpolatet a function from the data and am starting with the calculating of the points

--- src/pre_processing/interpolate.py
+++ src/pre_processing/interpolate.py
@@ -1,2 +1,20 @@
 import numpy as np
 from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
+hall_sensor_messurment_mv = [2671, 2640, 2560, 2511, 2490]
+
+curve_radius_mm = [ 295.4, 360, 0,-360, -295.4] 
+#positiv values indicate a left turn, negativ values indicate a right turn
+
+function=interp1d(hall_sensor_messurment_mv, curve_radius_mm, kind='cubic', fill_value='extrapolate')
+
+
+x = np.linspace(2671, 2490, 100)
+#x = np.linspace(2700, 2450, 100)
+y = function(x)
+
+function(4000)
+
+plt.scatter(x,y)
+plt.show()
